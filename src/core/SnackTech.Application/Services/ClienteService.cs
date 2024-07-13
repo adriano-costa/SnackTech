ae8ba85dcf7b3df0f789a3d12e7be872cc230dcc
Adriano Costa <adriano.dmcosta@gmail.com>
2024-07-13 11:04:21 -0300

    Criar services de produto e cliente na camada Application

---
+++ src/core/SnackTech.Application/Services/ClienteService.cs
@@ -0,0 +1,24 @@
+﻿using SnackTech.Domain.Contracts;
+using SnackTech.Domain.Models;
+
+namespace SnackTech.Application.Services;
+
+public class ClienteService
+{
+    private readonly IClienteRepository clienteRepository;
+
+    public ClienteService(IClienteRepository clienteRepository)
+    {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public void Save(Cliente cliente)
+    {
+        clienteRepository.Save(cliente);
+    }
+
+    public Cliente Get(string cpf)
+    {
+        return clienteRepository.Get(cpf);
+    }
+}
---
+++ src/core/SnackTech.Application/Services/ProdutoService.cs
@@ -0,0 +1,35 @@
+﻿using SnackTech.Domain.Contracts;
+using SnackTech.Domain.Enums;
+using SnackTech.Domain.Models;
+
+namespace SnackTech.Application.Services;
+
+public class ProdutoService
+{
+    private readonly IProdutoRepository produtoRepository;
+
+    public ProdutoService(IProdutoRepository produtoRepository)
+    {
+        this.produtoRepository = produtoRepository;
+    }
+
+    public void Save(Produto produto)
+    {
+        produtoRepository.Save(produto);
+    }
+
+    public Produto Get(Guid id)
+    {
+        return produtoRepository.Get(id);
+    }
+
+    public void Delete(Guid id)
+    {
+        produtoRepository.Delete(id);
+    }
+
+    public IEnumerable<Produto> GetByCategoria(CategoriaProduto categoriaProduto)
+    {
+        return produtoRepository.GetByCategoria(categoriaProduto);
+    }
+}
