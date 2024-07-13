af6d922c05e160894970bc79f80fee8c12271911
Adriano Costa <adriano.dmcosta@gmail.com>
2024-07-13 11:04:44 -0300

    Testes iniciais da camada service

---
+++ src/tests/SnackTech.Application.Tests/ServicesTests/ClienteServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using SnackTech.Application.Services;
+using SnackTech.Domain.Contracts;
+using SnackTech.Domain.Guards;
+using SnackTech.Domain.Models;
+using Telerik.JustMock;
+using Telerik.JustMock.Helpers;
+
+namespace SnackTech.Application.Tests.ServicesTests;
+
+public class ClienteServiceTest
+{
+    [Fact]
+    public void SaveClienteWithValidData()
+    {
+        var cliente = new Cliente("Joao", "aaa@teste.com", "12345678909");
+        IClienteRepository mockRepo = Mock.Create<IClienteRepository>();
+        Mock.Arrange(() => mockRepo.Save(cliente)).OccursOnce();
+
+        var service = new ClienteService(mockRepo);
+
+        service.Save(cliente);
+
+        Mock.Assert(mockRepo);
+    }
+}
