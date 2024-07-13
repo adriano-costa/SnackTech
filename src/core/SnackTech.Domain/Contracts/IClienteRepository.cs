1d70b377ebd96503b9f6a020b848c43665420374
Adriano Costa <adriano.dmcosta@gmail.com>
2024-07-13 11:03:52 -0300

    Criar contratos de repositorios de produto e cliente

---
+++ src/core/SnackTech.Domain/Contracts/IClienteRepository.cs
@@ -0,0 +1,9 @@
+﻿using SnackTech.Domain.Models;
+
+namespace SnackTech.Domain.Contracts;
+
+public interface IClienteRepository
+{
+    public void Save(Cliente cliente);
+    public Cliente Get(string cpf);
+}
---
+++ src/core/SnackTech.Domain/Contracts/IProdutoRepository.cs
@@ -0,0 +1,12 @@
+﻿using SnackTech.Domain.Enums;
+using SnackTech.Domain.Models;
+
+namespace SnackTech.Domain.Contracts;
+
+public interface IProdutoRepository
+{
+    public void Save(Produto produto);
+    public Produto Get(Guid id);
+    public void Delete(Guid id);
+    public IEnumerable<Produto> GetByCategoria(CategoriaProduto categoriaProduto);
+}
